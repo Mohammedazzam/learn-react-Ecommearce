6541adc4d1d7619a1c47bf4a03cd03d558ed8664
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2022-05-24 23:10:12 +0300

    custome Hooks

--- src/customHooks/Fetchadta.js
+++ src/customHooks/Fetchadta.js
@@ -1,9 +1,11 @@
-import React , {useEffect, useState} from "react"
+import React, {
+    useEffect,
+    useState
+} from "react"
 export const useFetchdata = (url) => {
     const [state, setstate] = useState([])
     useEffect(() => {
-        fetch(url).then
-        (res => res.json()).then(data=>setstate(data))
+        fetch(url).then(res => res.json()).then(data => setstate(data))
     }, [])
     return [state]
 }
